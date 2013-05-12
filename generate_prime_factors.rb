30ae754c8e23eed5f31dd2efb4ab1a4d7e4527db
Richard Wilson <r.crawfordwilson@gmail.com>
2013-05-12 12:41:17 -0700

    This one was hard..

---
+++ generate_prime_factors.rb
@@ -0,0 +1,22 @@
+#Ruby solution for http://projecteuler.net/problem=2
+#Prepared by Richard Wilson (Senjai)
+
+require 'prime' #So we have to do less work
+
+#We'll keep to our functional style of approaching these problems.
+
+def gen_prime_factors(num, start)
+  while (num >= start) do
+    if (start.prime? && (num % start == 0))
+      best = start
+      num /= start
+      puts "Prime! #{start}"
+    else
+      start += 2
+    end
+  end
+  best
+end
+
+puts gen_prime_factors(600851475143, 3)
+
